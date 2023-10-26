de97071d4d82a75fbee74d470eb1a5d565a252e9
Tachibana Kanade <h0m54r@mastodon.social>
2023-10-26 13:51:40 +0100

    add debug option

--- justgiving_totaliser/__main__.py
+++ justgiving_totaliser/__main__.py
@@ -1,3 +1,10 @@
+import logging
+import sys
+
 from .justgiving_totaliser import main
+
+if len(sys.argv) > 0 and sys.argv[1] == "debug":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
 main()
